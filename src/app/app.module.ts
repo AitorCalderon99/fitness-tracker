1e47e7a0ba1778fdf5be9131407f521abb0dbb6a
Aitor Calderon <aitorclzamakola@gmail.com>
2023-09-13 10:48:53 +0200

    Add training folder with training, current-training, new-training and past-trainings components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MaterialModule} from "./material.module";
 import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
+import { TrainingComponent } from './training/training.component';
+import { CurrentTrainingComponent } from './training/current-training/current-training.component';
+import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
+import { NewTrainingComponent } from './training/new-training/new-training.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
-    LoginComponent
+    LoginComponent,
+    TrainingComponent,
+    CurrentTrainingComponent,
+    PastTrainingsComponent,
+    NewTrainingComponent
   ],
   imports: [
     BrowserModule,
