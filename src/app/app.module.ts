b7497a3a57d9d7fbc7c62325e414936884855e03
Aitor Calderon <aitorclzamakola@gmail.com>
2023-09-13 10:56:03 +0200

    Add initial routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from "./material.module";
-import { SignupComponent } from './auth/signup/signup.component';
-import { LoginComponent } from './auth/login/login.component';
-import { TrainingComponent } from './training/training.component';
-import { CurrentTrainingComponent } from './training/current-training/current-training.component';
-import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
-import { NewTrainingComponent } from './training/new-training/new-training.component';
-import { WelcomeComponent } from './welcome/welcome.component';
+import {SignupComponent} from './auth/signup/signup.component';
+import {LoginComponent} from './auth/login/login.component';
+import {TrainingComponent} from './training/training.component';
+import {CurrentTrainingComponent} from './training/current-training/current-training.component';
+import {PastTrainingsComponent} from './training/past-trainings/past-trainings.component';
+import {NewTrainingComponent} from './training/new-training/new-training.component';
+import {WelcomeComponent} from './welcome/welcome.component';
+import {AppRoutingModule} from "./app.routing.module";
 
 @NgModule({
   declarations: [
@@ -25,9 +26,11 @@ import { WelcomeComponent } from './welcome/welcome.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
