0dadf8d351ecd03c03ffcf4a11195348a41b7bed
Aitor Calderon <aitorclzamakola@gmail.com>
2023-09-16 15:20:44 +0200

    Starting training component, tabs for children

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,9 +13,8 @@ import {WelcomeComponent} from './welcome/welcome.component';
 import {AppRoutingModule} from "./app.routing.module";
 import {FlexLayoutModule} from "@angular/flex-layout";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { HeaderComponent } from './navigation/header/header.component';
-import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
-
+import {HeaderComponent} from './navigation/header/header.component';
+import {SidenavListComponent} from './navigation/sidenav-list/sidenav-list.component';
 
 @NgModule({
   declarations: [
@@ -37,7 +36,7 @@ import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.com
     AppRoutingModule,
     FlexLayoutModule,
     FormsModule,
-    ReactiveFormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
