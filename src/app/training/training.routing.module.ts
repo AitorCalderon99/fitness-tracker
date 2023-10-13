369f3ab9e9a43002837fe54a85182d4e9115494b
Aitor Calderon <aitorclzamakola@gmail.com>
2023-10-13 13:45:23 +0200

    refactor(training): Moving training guard

--- src/app/training/training.routing.module.ts
+++ src/app/training/training.routing.module.ts
@@ -1,10 +1,9 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {TrainingComponent} from "./training.component";
-import {trainingGuard} from "../auth/auth.guard";
 
 const routes: Routes = [
-  {path: '', component: TrainingComponent, canActivate: [trainingGuard]},
+  {path: '', component: TrainingComponent },
 ];
 
 @NgModule({
