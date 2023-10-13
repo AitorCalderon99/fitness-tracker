19ade25e275def1462dccef72311c11cd66953d4
Aitor Calderon <aitorclzamakola@gmail.com>
2023-10-13 11:00:19 +0200

    refactor(Auth): Create auth.module splitting app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from "./material.module";
-import {SignupComponent} from './auth/signup/signup.component';
-import {LoginComponent} from './auth/login/login.component';
 import {TrainingComponent} from './training/training.component';
 import {CurrentTrainingComponent} from './training/current-training/current-training.component';
 import {PastTrainingsComponent} from './training/past-trainings/past-trainings.component';
@@ -12,7 +10,6 @@ import {NewTrainingComponent} from './training/new-training/new-training.compone
 import {WelcomeComponent} from './welcome/welcome.component';
 import {AppRoutingModule} from "./app.routing.module";
 import {FlexLayoutModule} from "@angular/flex-layout";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HeaderComponent} from './navigation/header/header.component';
 import {SidenavListComponent} from './navigation/sidenav-list/sidenav-list.component';
 import {StopTrainingComponent} from "./training/current-training/stop-training.component";
@@ -23,13 +20,12 @@ import {environment} from "../environments/environment";
 import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
 import {AngularFireAuthModule} from "@angular/fire/compat/auth";
 import {UiService} from "./shared/ui.service";
+import {AuthModule} from "./auth/auth.module";
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent,
-    LoginComponent,
     TrainingComponent,
     CurrentTrainingComponent,
     PastTrainingsComponent,
@@ -45,11 +41,10 @@ import {UiService} from "./shared/ui.service";
     MaterialModule,
     AppRoutingModule,
     FlexLayoutModule,
-    FormsModule,
-    ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    AuthModule
   ],
   providers: [AuthService, TrainingService, UiService],
   bootstrap: [AppComponent]
